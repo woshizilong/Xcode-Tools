c277d0ac3870c732004735450e20dde1f5f3ace2
woshizilong <woshizilong@hotmail.com>
2015-02-24 12:37:45 +0800

    install vundle (vim plugin manager)

--- setup_Xcode-Tools.sh
+++ setup_Xcode-Tools.sh
@@ -8,5 +8,10 @@ ln -s ${SRC_HOME}/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
 ln -s ${SRC_HOME}/FontAndColorThemes ~/Library/Developer/Xcode/UserData/FontAndColorThemes
 ln -s ${SRC_HOME}/Plug-ins ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins
 
+#install vundle (vim plugin manager)
+#mkdir .vim
+#touch .vimrc (copy from git)
+git clone https://github.com/woshizilong/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 echo "done"
 
