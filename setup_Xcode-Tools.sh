8599f9d89b765ae3be2a587f8bb621b955630c05
woshizilong <woshizilong@hotmail.com>
2015-10-29 19:07:59 +0800

    升级Xcode后插件失效解决办法

--- setup_Xcode-Tools.sh
+++ setup_Xcode-Tools.sh
@@ -23,11 +23,10 @@ ln -s ${SRC_HOME}/Plug-ins ~/Library/Application\ Support/Developer/Shared/Xcode
 #./install.sh --clang-completer
 
 #升级Xcode6.3插件失效解决办法
-#1.打开终端，输入以下代码获取到DVTPlugInCompatibilityUUID
-#defaults read /Applications/Xcode.app/Contents/Info DVTPlugInCompatibilityUUID
-#2.然后输入如下命令   【最后一项是获取到的DVTPlugInCompatibilityUUID】
-#find ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins -name Info.plist -maxdepth 3 | xargs -I{} defaults write {} DVTPlugInCompatibilityUUIDs -array-add 9F75337B-21B4-4ADC-B558-F9CADF7073A7
+#1.打开终端，输入以下命令   【最后一项是获取到的DVTPlugInCompatibilityUUID】
+#find ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins -name Info.plist -maxdepth 3 | xargs -I{} defaults write {} DVTPlugInCompatibilityUUIDs -array-add `defaults read /Applications/Xcode.app/Contents/Info.plist DVTPlugInCompatibilityUUID`
 
+#2.如果没起作用将所有插件移除Plug-ins目录，重启Xcode后关闭，再将所有插件移回Plug-ins目录,重启Xcode，提示框中选择Load
 
 echo "done"
 
